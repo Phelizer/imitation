3e38cb84ddc1eda53be90a76ece799bed77811e4
Alex Chornyi <chornyi.oleksandr@lll.kpi.ua>
2022-12-27 13:27:09 +0200

    Add stats calculations

--- src/queue.ts
+++ src/queue.ts
@@ -4,6 +4,29 @@ export class Queue<T> {
   private _capacity: number;
   private _elements: T[];
 
+  private queueLengthes: number[] = [];
+  private totalQueries: number = 0;
+  private handledQueries: number = 0;
+  private lostQueries: number = 0;
+
+  private get averageQueueLength() {
+    return R.mean(this.queueLengthes);
+  }
+
+  private get probabilityOfHandling() {
+    return this.handledQueries / this.totalQueries;
+  }
+
+  get stats() {
+    return {
+      averageQueueLength: this.averageQueueLength,
+      probabilityOfHandling: this.probabilityOfHandling,
+      lostQueries: this.lostQueries,
+      handledQueries: this.handledQueries,
+      totalQueries: this.totalQueries,
+    };
+  }
+
   get capacity() {
     return this._capacity;
   }
@@ -17,7 +40,10 @@ export class Queue<T> {
   }
 
   enqueue(element: T) {
+    this.totalQueries++;
+
     if (this._elements.length >= this._capacity) {
+      this.lostQueries++;
       return;
     }
 
@@ -25,11 +51,18 @@ export class Queue<T> {
   }
 
   dequeue() {
+    this.handledQueries++;
     return this._elements.shift();
   }
 
   constructor(capacity: number, ...elements: T[]) {
     this._capacity = capacity;
     this._elements = elements;
+
+    // Each 20 ms it will push the current queue length to the queueLengthes array
+    // in order to calculate the average queue length
+    setInterval(() => {
+      this.queueLengthes.push(this.length);
+    }, 20);
   }
 }
